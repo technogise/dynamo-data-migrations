fe30d22a10129e08cb0175dc631fd70ecd266038
Smriti Singh <smriti2815@gmail.com>
2022-11-27 21:47:37 +0530

    updated moduleLoader.spec.ts

--- tests/lib/utils/moduleLoader.spec.ts
+++ tests/lib/utils/moduleLoader.spec.ts
@@ -1,12 +1,26 @@
+import * as tsImport from 'ts-import';
 import * as moduleLoader from "../../../src/lib/utils/moduleLoader";
 
 describe("moduleLoader",()=>{
+    let tsImportLoad: { mockRestore: () => void; };
+
+    beforeEach(async()=>{
+        tsImportLoad = jest.spyOn(tsImport,"load");
+    });
+
+    afterEach(async()=>{
+        tsImportLoad.mockRestore();
+    });
 
     describe("importFile()",()=>{
 
-        it("should call the import object",async()=>{
-            await moduleLoader.importFile("abc");
-            expect(1).toEqual(1);
+        it("should call tsImport.load()",async()=>{
+            const fakeMigrationFileName = "migrations/abc.ts";
+            const fakeReturnValue = {};
+            const load = jest.spyOn(tsImport,"load").mockResolvedValue(fakeReturnValue);
+            
+            await moduleLoader.importFile(fakeMigrationFileName);
+            expect(load).toBeCalled();
         })
     })
 })
