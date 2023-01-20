bf08f5dabc37db11dd580ad0a860bd6ad8728b18
Mukund <mukund@technogise.com>
2023-01-20 12:27:52 +0530

    Refactoring: 1) Updated comments for secretAcessKey

--- src/samples/config.ts
+++ src/samples/config.ts
@@ -6,6 +6,6 @@ export const awsConfig = [
         profile: '', // Can be a value to denote env. Eg. dev,test,prod. If not provided it is considered to be default
         region: '', // Mandatory to be provided for each profile
         accessKeyId: '', // Optional. If not provided credentials are taken from shared credentials file.
-        secretAccessKey: '',
+        secretAccessKey: '', // Optional. If not provided credentials are taken from shared credentials file.
     },
 ];
